44f08af353530ee3f1705ab16cfba294de911b90
RigelStar <47748256+rigel-star@users.noreply.github.com>
2020-04-28 23:19:24 +0545

    removed connectionManager class

--- src/application/MainBackgroundPane.java
+++ src/application/MainBackgroundPane.java
@@ -1,17 +1,23 @@
 package application;
 
+import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Component;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Point;
 import java.awt.event.ComponentEvent;
 import java.awt.event.ComponentListener;
-import java.awt.image.BufferedImage;
+import java.awt.geom.QuadCurve2D;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.swing.JComponent;
 import javax.swing.JDesktopPane;
 
 import org.lemon.image.ImageView;
-import org.lemon.utils.ConnectionManager;
+import org.lemon.math.Vec2d;
+import org.lemon.utils.Pair;
 
 /**
  * Main background pane of application, which handles editing and drawing panels in application.
@@ -19,37 +25,19 @@ import org.lemon.utils.ConnectionManager;
 public class MainBackgroundPane extends JDesktopPane implements ComponentListener {
 	private static final long serialVersionUID = 1L;
 	
-	/*storing pairs*/
-	private List<ConnectionManager.Pair> connections = new ArrayList<>();
 
-	public MainBackgroundPane() {
-		this(null, null);
-	}
+	/*Self pointing*/
+	MainBackgroundPane 			self = this;
 	
-	public MainBackgroundPane(BufferedImage img, String title) {
+	public MainBackgroundPane() {
 		
 		setSize(300, 300);
 		setBackground(Color.gray);
 		setVisible(true);
 		addComponentListener(this);
-		
 	}
 	
 	
-	/**
-	 * Make connection between pair of nodes.
-	 * This method used to draw line or curve between two connected nodes.
-	 * <p>
-	 * @param pair of nodes
-	 * */
-	public void makeConnection(ConnectionManager.Pair pair) {
-		connections.add(pair);
-		new ConnectionManager(this, connections);
-	}
-	
-	public void refreshConnections() {
-		System.out.println("Refreshing");
-	}
 	
 	/**
 	 * Refresh's all the available ImageViews.
@@ -61,8 +49,12 @@ public class MainBackgroundPane extends JDesktopPane implements ComponentListene
 		
 		for(Component c: getComponents()) {
 			if(c instanceof ImageView) {
-				views.add((ImageView) c);
-				titles.add(((ImageView)c).getTitle());
+				
+				ImageView view = (ImageView) c;
+				
+				view.addComponentListener(this);
+				views.add((ImageView) view);
+				titles.add(((ImageView) view).getTitle());
 			}
 		}
 		
@@ -71,6 +63,50 @@ public class MainBackgroundPane extends JDesktopPane implements ComponentListene
 		}
 		
 	}
+	
+	
+	@Override
+	public void paint(Graphics g) {
+		super.paint(g);
+		
+		for(Component c: getComponents()) {
+			if(c instanceof ImageView) {
+				ImageView view = (ImageView) c;
+				if(view.getConnection() != null) {
+					draw(g, new Pair(view, view.getConnection()));
+				}
+			}
+		}
+		
+	}
+	
+	
+	void draw(Graphics g, org.lemon.utils.Pair p) {
+		Graphics2D g2d = (Graphics2D) g;
+		g2d.setPaint(Color.white);
+		g2d.setStroke(new BasicStroke(3));
+		
+		Vec2d v1 = p.getFirstPoint();
+		Vec2d v2 = p.getSecondPoint();
+		JComponent start = p.getFirstComponent(), end = p.getSecondComponent();
+		
+		Point begin = new Point(v1.x + start.getWidth(),
+				v1.y + (start.getHeight() / 2));
+		Point stop = new Point(v2.x,
+				v2.y + (end.getHeight() / 2));
+		
+		Vec2d ctrlPoint = v1.midpoint(v2);
+		
+		QuadCurve2D.Double curve = new QuadCurve2D.Double(begin.x, begin.y,
+															ctrlPoint.x, ctrlPoint.y,
+																stop.x, stop.y);
+		g2d.fillOval(begin.x - 5, begin.y, 10, 10);
+		g2d.fillOval(stop.x - 5, stop.y, 10, 10);
+		g2d.draw(curve);
+		
+	}
+	
+	
 
 	@Override
 	public void componentResized(ComponentEvent e) {
@@ -79,7 +115,7 @@ public class MainBackgroundPane extends JDesktopPane implements ComponentListene
 
 	@Override
 	public void componentMoved(ComponentEvent e) {
-		refreshConnections();
+		repaint();
 	}
 
 	@Override
