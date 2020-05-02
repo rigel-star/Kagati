d5df60eb9a51fb51ebb25bbb3c55f1e38673c47d
RigelStar <47748256+rigel-star@users.noreply.github.com>
2020-05-02 22:54:04 +0545

    location change

---
+++ src/org/lemon/image/filters/basic_filters/GrayScale.java
@@ -0,0 +1,42 @@
+package org.lemon.image.filters.basic_filters;
+
+import java.awt.Color;
+import java.awt.image.BufferedImage;
+
+public class GrayScale {
+
+	BufferedImage img;
+	
+	public GrayScale() {
+		
+	}
+	
+	public GrayScale(BufferedImage img) {
+		this.img = img;
+		int height = img.getHeight();
+		int width = img.getWidth();
+		
+		for(int y=0;y<height;y++) {
+			for(int x=0;x<width;x++) {
+				
+				Color color = new Color(img.getRGB(x, y));
+				
+				//equation is R*0.29 + B*0.58 + B*0.11
+				int R = (int) (color.getRed() * 0.29);
+				int G = (int) (color.getGreen() * 0.58);
+				int B = (int) (color.getBlue() * 0.11);
+				
+				Color newColor = new Color(R+G+B, R+G+B, R+G+B);
+				
+				img.setRGB(x, y, newColor.getRGB());
+				
+			}
+		}
+		
+	}
+	
+	public BufferedImage getGrayScaledImg() {
+		return img;
+	}
+	
+}
