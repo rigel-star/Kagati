dcea30218959d82a93c71afc4dde7068d1565531
RigelStar <47748256+rigel-star@users.noreply.github.com>
2020-05-09 23:18:08 +0545

    Add files via upload

---
+++ src/org/lemon/gui/panels/ImageAnalyzePanel.java
@@ -0,0 +1,28 @@
+package org.lemon.gui.panels;
+
+import java.awt.Color;
+
+import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
+import javax.swing.JPanel;
+import javax.swing.border.Border;
+
+public class ImageAnalyzePanel extends JPanel {
+
+	//version id
+	private static final long serialVersionUID = 1L;
+	
+	public ImageAnalyzePanel() {
+		
+		//layout for panel
+		BoxLayout layout = new BoxLayout(this, BoxLayout.Y_AXIS);
+        
+		//border for panel
+		Border layoutBorder = BorderFactory.createLineBorder(Color.BLACK, 2);
+		
+		setLayout(layout);
+		setBorder(layoutBorder);
+		
+
+	}
+}
