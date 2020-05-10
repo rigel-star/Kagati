b79b3399d19517c413ffef587c7bec020d782acb
RigelStar <47748256+rigel-star@users.noreply.github.com>
2020-05-10 20:00:35 +0545

    Add files via upload

--- src/org/lemon/gui/panels/LemonToolsMenu.java
+++ src/org/lemon/gui/panels/LemonToolsMenu.java
@@ -9,15 +9,14 @@ import java.awt.event.ActionListener;
 import javax.swing.BoxLayout;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JComponent;
 import javax.swing.JInternalFrame;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
-import org.lemon.gui.drawing.PlainDrawingPanel;
-import org.lemon.gui.image.ImagePanel;
-import org.lemon.gui.image.LemonImageView;
-import org.lemon.gui.image.ImagePanel.PanelMode;
-import org.piksel.piksel.PPInternalWindow;
+import org.lemon.gui.drawing.image.DrawingCanvasOnImage;
+import org.lemon.gui.image.ImageView;
+import org.lemon.tools.select.PolygonalSelectTool;
 
 import application.MainApplicationScene;
 
@@ -26,15 +25,22 @@ public class LemonToolsMenu extends JInternalFrame implements ActionListener{
 
 	private static final long serialVersionUID = 1L;
 	
-	private JButton 		brushTool;
-	private JButton 		snappingTool;
+	private JButton 					brushTool;
+	private JButton 					polySnappingTool;
+	private JButton						cropTool;
+	
+	
+	
+	private PolygonalSelectTool 		polyToolListener;
+	private DrawingCanvasOnImage		brushToolListener;
+
 	
 	
 	//panels
-	JPanel right;
+	private JPanel 						right;
 	
-	private Component 		target;
-	MainApplicationScene 		parent;
+	private Component 					target;
+	MainApplicationScene 				parent;
 	
 	/**
 	 * Tools menu for editing all sorts of canvas on application.
@@ -59,45 +65,58 @@ public class LemonToolsMenu extends JInternalFrame implements ActionListener{
 		setBackground(Color.white);
 		setLocation(30, 40);
 		
-		init();
+		addAll();
 		initListeners();
 		
 		Container c = getContentPane();
 		c.add(right);
 		
-		right.add(brushTool);
-		right.add(snappingTool);
-		
 	}
 	
 	
-	/*init all components*/
-	private void init() {
-		
-		//default brush tool
-		brushTool = new JButton();
+	/*add all buttons (tools) to menu*/
+	private void addAll() {
+		right.add(createBrushTool());
+		right.add(createPolySelectTool());
+		right.add(createCropTool());
+	}
+	
+	
+	/*create polygonal selection tool button*/
+	private JButton createPolySelectTool() {
+		this.polySnappingTool = new JButton();
+		polySnappingTool.setIcon(new ImageIcon("icons/tools/cutter.png"));
+		return polySnappingTool;
+	}
+	
+	
+	/*create brush tool button*/
+	private JButton createBrushTool() {
+		this.brushTool = new JButton();
 		brushTool.setIcon(new ImageIcon("icons/tools/brush.png"));
-		
-		//image snapping tool i.e cutter tool
-		snappingTool = new JButton();
-		snappingTool.setIcon(new ImageIcon("icons/tools/cutter.png"));
-		
+		return brushTool;
+	}
+	
+	
+	/*create crop tool button*/
+	private JButton createCropTool() {
+		this.cropTool = new JButton();
+		cropTool.setIcon(new ImageIcon("icons/tools/crop.png"));
+		return cropTool;
 	}
 	
 	
 	/*init all components listeners*/
 	private void initListeners() {
 		this.brushTool.addActionListener(this);
-		this.snappingTool.addActionListener(this);
+		this.polySnappingTool.addActionListener(this);
 	}
 
 	
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		
-		LemonImageView view = null;
-		PlainDrawingPanel draw = null;
-		PPInternalWindow pixel = null;
+		ImageView view = null;
 		
 		if(parent == null)
 			return;
@@ -110,30 +129,70 @@ public class LemonToolsMenu extends JInternalFrame implements ActionListener{
 			}
 		}
 		
-		if(target instanceof LemonImageView) {
-			view = (LemonImageView) target;
-			
-			System.out.println(view.getImagePanel().getPanelMode());
-			ImagePanel pan = view.getImagePanel();
-			pan.setPanelMode(PanelMode.CANVAS_MODE);
-			pan.revalidate();
-			
-			System.out.println(view.getImagePanel().getPanelMode());
-			return;
-		} 
-		else if(target instanceof PlainDrawingPanel) {
-			draw = (PlainDrawingPanel) target;
-			draw.setTitle("New title");
-			draw.revalidate();
-			return;
-		}
-		else if(target instanceof PPInternalWindow) {
-			pixel = (PPInternalWindow) target;
-			pixel.setTitle("Hello shit");
-			pixel.revalidate();
-			return;
+		if(e.getSource() == this.brushTool) {
+			if(target instanceof ImageView) {
+				view = (ImageView) target;
+				var pan = view.getImagePanel();
+				brushToolListener = new DrawingCanvasOnImage(pan);
+				ifImageView(pan, brushToolListener);
+				view.revalidateListeners();
+				return;
+			} 
 		}
 		
+		else if(e.getSource() == this.polySnappingTool) {
+			if(target instanceof ImageView) {
+				view = (ImageView) target;
+				var pan = view.getImagePanel();
+				polyToolListener = new PolygonalSelectTool(pan.getImage(), pan);
+				ifImageView(pan, polyToolListener);
+				view.revalidateListeners();
+				return;
+			}
+		}
+		
+	}
+	
+	
+	/*if the context is imageview*/
+	private void ifImageView(JComponent context, Object adp) {
+		removeAllListeners(context);
+		
+		if(adp instanceof DrawingCanvasOnImage) {
+			applyBrushTool(context, (DrawingCanvasOnImage) adp);
+		}
+		else if(adp instanceof PolygonalSelectTool) {
+			applyPolySelectTool(context, (PolygonalSelectTool) adp);
+		}
+		
+		context.revalidate();
+	}
+	
+	
+	/*apply brush tool to the specific container*/
+	private void applyBrushTool(JComponent container, DrawingCanvasOnImage tool) {
+		container.addMouseMotionListener(tool);
+	}
+	
+	
+	/*apply poly snapping tool to the specific container*/
+	private void applyPolySelectTool(JComponent container, PolygonalSelectTool tool) {
+		container.addMouseListener(tool);
+		container.addMouseMotionListener(tool);
+	}
+	
+	
+	/*remove each and every mouse listener which is applied to the specific container*/
+	private void removeAllListeners(JComponent container) {
+		var l1 = container.getMouseListeners();
+		var l2 = container.getMouseMotionListeners();
+		
+		for(int i=0; i<l1.length; i++) {
+			container.removeMouseListener(l1[i]);
+		}
+		for(int i=0; i<l2.length; i++) {
+			container.removeMouseMotionListener(l2[i]);
+		}
 	}
 
 }
