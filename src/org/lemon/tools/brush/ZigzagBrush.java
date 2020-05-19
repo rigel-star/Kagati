0e767bab3e141aa8000ea9cb10fb2fc9b9f6f862
RigelStar <47748256+rigel-star@users.noreply.github.com>
2020-05-19 23:31:27 +0545

    zigzag brush

---
+++ src/org/lemon/tools/brush/ZigzagBrush.java
@@ -0,0 +1,67 @@
+package org.lemon.tools.brush;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.Stroke;
+import java.awt.geom.Line2D;
+
+public class ZigzagBrush implements BrushTool {
+
+	
+	
+	private int strokeSize = BrushTool.defaultStrokeSize;
+	private Color strokeColor = BrushTool.defaultStrokeColor;
+	private Stroke stroke;
+	
+	private Graphics2D context;
+	
+	
+	public ZigzagBrush(Graphics2D context) {
+		
+		this.context = context;
+		
+		if(strokeSize == 0)
+			strokeSize = 1;
+		
+		stroke = new ZigzagStroke(defaultStroke, strokeSize, strokeSize);
+		context.setStroke(stroke);
+	}
+	
+	@Override
+	public void draw(int newX, int newY, int oldX, int oldY) {
+		context.draw(new Line2D.Double(newX, newY, oldX, oldY));
+	}
+	
+	
+	
+	@Override
+	public void setStrokeSize(int size) {
+		this.strokeSize = size;
+	}
+
+	
+	@Override
+	public int getStrokeSize() {
+		return strokeSize;
+	}
+
+	
+	@Override
+	public void setStrokeColor(Color color) {
+		this.strokeColor = color;
+	}
+
+	
+	@Override
+	public Color getStrokeColor() {
+		return strokeColor;
+	}
+
+	@Override
+	public Stroke getStroke() {
+		return stroke;
+	}
+
+	
+
+}
