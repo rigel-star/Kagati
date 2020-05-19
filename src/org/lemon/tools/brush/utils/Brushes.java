164fa3731220a4ffda92b21091e65c8e8d36f988
RigelStar <47748256+rigel-star@users.noreply.github.com>
2020-05-19 23:05:32 +0545

    Add files via upload

--- src/org/lemon/tools/brush/utils/Brushes.java
+++ src/org/lemon/tools/brush/utils/Brushes.java
@@ -5,6 +5,7 @@ import java.awt.Graphics2D;
 import org.lemon.tools.brush.BrushTool;
 import org.lemon.tools.brush.NormalBrushTool;
 import org.lemon.tools.brush.SoftBrushTool;
+import org.lemon.tools.brush.WobbleBrush;
 
 public class Brushes {
 	
@@ -14,5 +15,9 @@ public class Brushes {
 	
 	public static BrushTool createSoftBrush(Graphics2D context) {
 		return new SoftBrushTool(context);
+	}
+	
+	public static BrushTool createWobbleBrush(Graphics2D context) {
+		return new WobbleBrush(context);
 	}
 }
