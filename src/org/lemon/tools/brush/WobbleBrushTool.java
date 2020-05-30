68ba50ee2d53204d4361d2b54a53ecb6df03a221
RigelStar <47748256+rigel-star@users.noreply.github.com>
2020-05-30 13:10:17 +0545

    name change of brushes and minor fixes

--- src/org/lemon/tools/brush/WobbleBrushTool.java
+++ src/org/lemon/tools/brush/WobbleBrushTool.java
@@ -1,15 +1,14 @@
 package org.lemon.tools.brush;
 
-import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Graphics2D;
-import java.awt.RenderingHints;
 import java.awt.Stroke;
 import java.awt.geom.Line2D;
 
 import org.lemon.tools.BrushTool;
 
-public class SoftBrushTool implements BrushTool {
+public class WobbleBrushTool implements BrushTool {
+
 	
 	
 	private int strokeSize = BrushTool.defaultStrokeSize;
@@ -18,25 +17,18 @@ public class SoftBrushTool implements BrushTool {
 	
 	private Graphics2D context;
 	
-	/**
-	 * Soft Brush is brush with feature of anti aliasing. After brushing, the brushed part will be
-	 * anti aliased with surrounding.
-	 * @param context canvas to draw on
-	 * @param strokeColor color to draw with
-	 * @param strokeSize stroke size
-	 * */
-	public SoftBrushTool(Graphics2D context) {
+	
+	public WobbleBrushTool(Graphics2D context) {
+		
 		this.context = context;
-		RenderingHints rhs = new RenderingHints(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-		context.setRenderingHints(rhs);
-		context.setColor(getStrokeColor());
 		
 		if(strokeSize == 0)
 			strokeSize = 1;
 		
-		stroke = new BasicStroke(getStrokeSize(), BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND);
+		stroke = new WobbleStroke(getStrokeSize(), getStrokeSize(), getStrokeSize());
 		
-		context.setStroke(stroke);
+		this.context.setStroke(stroke);
+		
 	}
 	
 	
@@ -46,30 +38,34 @@ public class SoftBrushTool implements BrushTool {
 	}
 	
 	
+	
 	@Override
 	public void setStrokeSize(int size) {
 		this.strokeSize = size;
 	}
 
+	
 	@Override
 	public int getStrokeSize() {
 		return strokeSize;
 	}
 
+	
 	@Override
 	public void setStrokeColor(Color color) {
-		this.context.setColor(color);
+		this.strokeColor = color;
 	}
 
+	
 	@Override
 	public Color getStrokeColor() {
 		return strokeColor;
 	}
 
+	
 	@Override
 	public Stroke getStroke() {
 		return stroke;
 	}
-
 
 }
--- src/org/lemon/tools/brush/ZigzagBrushTool.java
+++ src/org/lemon/tools/brush/ZigzagBrushTool.java
@@ -1,15 +1,14 @@
 package org.lemon.tools.brush;
 
-import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Graphics2D;
-import java.awt.RenderingHints;
 import java.awt.Stroke;
 import java.awt.geom.Line2D;
 
 import org.lemon.tools.BrushTool;
 
-public class SoftBrushTool implements BrushTool {
+public class ZigzagBrushTool implements BrushTool {
+
 	
 	
 	private int strokeSize = BrushTool.defaultStrokeSize;
@@ -18,27 +17,17 @@ public class SoftBrushTool implements BrushTool {
 	
 	private Graphics2D context;
 	
-	/**
-	 * Soft Brush is brush with feature of anti aliasing. After brushing, the brushed part will be
-	 * anti aliased with surrounding.
-	 * @param context canvas to draw on
-	 * @param strokeColor color to draw with
-	 * @param strokeSize stroke size
-	 * */
-	public SoftBrushTool(Graphics2D context) {
+	
+	public ZigzagBrushTool(Graphics2D context) {
+		
 		this.context = context;
-		RenderingHints rhs = new RenderingHints(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-		context.setRenderingHints(rhs);
-		context.setColor(getStrokeColor());
 		
 		if(strokeSize == 0)
 			strokeSize = 1;
 		
-		stroke = new BasicStroke(getStrokeSize(), BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND);
-		
+		stroke = new ZigzagStroke(defaultStroke, strokeSize, strokeSize);
 		context.setStroke(stroke);
 	}
-	
 	
 	@Override
 	public void draw(int newX, int newY, int oldX, int oldY) {
@@ -46,21 +35,25 @@ public class SoftBrushTool implements BrushTool {
 	}
 	
 	
+	
 	@Override
 	public void setStrokeSize(int size) {
 		this.strokeSize = size;
 	}
 
+	
 	@Override
 	public int getStrokeSize() {
 		return strokeSize;
 	}
 
+	
 	@Override
 	public void setStrokeColor(Color color) {
-		this.context.setColor(color);
+		this.strokeColor = color;
 	}
 
+	
 	@Override
 	public Color getStrokeColor() {
 		return strokeColor;
@@ -71,5 +64,6 @@ public class SoftBrushTool implements BrushTool {
 		return stroke;
 	}
 
+	
 
 }
