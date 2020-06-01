7ec93f22a0d9e30401eec662884150b89f7ec423
RigelStar <47748256+rigel-star@users.noreply.github.com>
2020-06-01 22:25:57 +0545

    editable component

---
+++ src/org/lemon/gui/DynamicComponent.java
@@ -0,0 +1,14 @@
+package org.lemon.gui;
+
+import java.awt.Color;
+
+/**
+ * The component which can be edited.
+ * */
+public interface DynamicComponent {
+
+	
+	void setColor(Color newColor);
+	Color getColor();
+	
+}
