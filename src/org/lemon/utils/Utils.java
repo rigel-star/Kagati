3e75a5216fd34b71d3bf66e03de95c1ad0e4006d
RigelStar <47748256+rigel-star@users.noreply.github.com>
2020-06-21 00:22:14 +0545

    minor changes

--- src/org/lemon/utils/Utils.java
+++ src/org/lemon/utils/Utils.java
@@ -1,8 +1,17 @@
 package org.lemon.utils;
 
+import java.awt.image.BufferedImage;
+import java.awt.image.ColorModel;
+import java.awt.image.WritableRaster;
+
 public class Utils {
 
 	
-	
+	public static BufferedImage getImageCopy(BufferedImage sr) {
+		ColorModel cm = sr.getColorModel();
+		boolean iap = sr.isAlphaPremultiplied();
+		WritableRaster raster = sr.copyData(sr.getRaster().createCompatibleWritableRaster());
+		return new BufferedImage(cm, raster, iap, null);
+	}
 	
 }
