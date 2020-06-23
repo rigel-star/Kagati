0ed5e43fc81acb5d16fa882b2d2c5e960a2cdeb5
RigelStar <47748256+rigel-star@users.noreply.github.com>
2020-06-23 14:32:39 +0545

    application's all global properties

---
+++ src/org/lemon/AppGlobalProperties.java
@@ -0,0 +1,55 @@
+package org.lemon;
+
+import java.awt.Color;
+
+import org.lemon.gui.ApplicationFrame;
+import org.lemon.tools.LemonTool;
+
+
+/**
+ * This is the class which handles all the global properties of application.
+ * Like which color or tool is currently selected. This will only used once in application.
+ * Object is defined in {@code ApplicationFrame} class and from there, it will be circulated to
+ * all over the application. All the UI components of application will take it as a first parameter.
+ * */
+public class AppGlobalProperties {
+
+	
+	private Color 						gColor = Color.yellow;
+	private LemonTool					gTool;
+	
+	
+	private ApplicationFrame			parent;
+	
+	
+	public AppGlobalProperties(ApplicationFrame parent) {
+		this.parent = parent;
+	}
+	
+	
+	public void revalidate() {
+		parent.revalidate();
+	}
+	
+	
+	public Color getGLobalColor() {
+		return gColor;
+	}
+	
+	
+	public void setGlobalColor(Color c) {
+		this.gColor = c;
+	}
+	
+	
+	public LemonTool getGlobalTool() {
+		return gTool;
+	}
+	
+	
+	public void setGlobalTool(LemonTool t) {
+		this.gTool = t;
+	}
+	
+	
+}
