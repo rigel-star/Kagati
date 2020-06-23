e7fea6fbb8075eda4478af21fdedb935b03847bc
RigelStar <47748256+rigel-star@users.noreply.github.com>
2020-06-23 23:13:14 +0545

    Point3D class for projective transformation
    This class is heavily used in Vanishing Point Filter.

---
+++ src/org/lemon/math/Point3.java
@@ -0,0 +1,14 @@
+package org.lemon.math;
+
+public class Point3 {
+
+	double x;
+	double y;
+	double z;
+
+	public Point3(double x, double y, double z) {
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
+}
