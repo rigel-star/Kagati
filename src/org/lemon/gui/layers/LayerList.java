2a05464ff56a1191baf6eaa1e83f9adff462f1f9
RigelStar <47748256+rigel-star@users.noreply.github.com>
2020-07-04 00:06:22 +0545

    contains Layers

--- src/org/lemon/gui/layers/LayerList.java
+++ src/org/lemon/gui/layers/LayerList.java
@@ -1,17 +1,24 @@
 package org.lemon.gui.layers;
 
+
+import javax.swing.DefaultListModel;
 import javax.swing.JList;
 
-public class LayerList extends JList<LayerPanel> {
+public class LayerList extends JList<Layer> {
 	private static final long serialVersionUID = 1L;
 	
 	
+	static DefaultListModel<Layer> model = new DefaultListModel<>();
+	static LayerRenderer renderer = new LayerRenderer();
+	
 	public LayerList() {
-		
+		super(model);
+		setCellRenderer(renderer);
 	}
 	
 	
-	public void add(LayerPanel l) {
+	public void add(Layer l) {
+		model.addElement(l);
 		
 	}
 
