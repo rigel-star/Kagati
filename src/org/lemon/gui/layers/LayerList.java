66026b38c4e6f1ec36e2a69571371a02b1898c35
RigelStar <47748256+rigel-star@users.noreply.github.com>
2020-07-05 20:36:08 +0545

    update: can hold empty layers

--- src/org/lemon/gui/layers/LayerList.java
+++ src/org/lemon/gui/layers/LayerList.java
@@ -1,15 +1,21 @@
 package org.lemon.gui.layers;
 
 
+import java.util.Enumeration;
+
 import javax.swing.DefaultListModel;
 import javax.swing.JList;
+
+import org.lemon.utils.IDManager;
 
 public class LayerList extends JList<Layer> {
 	private static final long serialVersionUID = 1L;
 	
 	
-	static DefaultListModel<Layer> model = new DefaultListModel<>();
-	static LayerRenderer renderer = new LayerRenderer();
+	private static DefaultListModel<Layer> model = new DefaultListModel<>();
+	private static LayerRenderer renderer = new LayerRenderer();
+	private IDManager<Layer> idMngr = new IDManager<Layer>();
+	
 	
 	public LayerList() {
 		super(model);
@@ -17,9 +23,30 @@ public class LayerList extends JList<Layer> {
 	}
 	
 	
+	public void refresh() {
+		for(Enumeration<Layer> enu = model.elements(); enu.hasMoreElements();) {
+			Layer layer  = enu.nextElement();
+			layer.getLayerComponent().repaint();
+		}
+	}
+	
+	
+	public int getLayerCount() {
+		return model.size();
+	}
+	
+	
 	public void add(Layer l) {
 		model.addElement(l);
-		
+		int id = idMngr.next();
+		l.id = id;
+		idMngr.add(id, l);
 	}
+	
+	
+	public void remove(Layer l) {
+		model.removeElement(l);
+	}
+	
 
 }
