91dceed92e91410ec1ceef822300d1f10334ff30
RigelStar <47748256+rigel-star@users.noreply.github.com>
2020-08-06 22:36:07 +0545

    lemon object annotation

---
+++ src/org/lemon/LemonObject.java
@@ -0,0 +1,15 @@
+package org.lemon;
+
+import java.lang.annotation.Documented;
+
+@Documented
+public @interface LemonObject {
+	
+	static String GUI_CLASS = "type-container";
+	static String HELPER_CLASS = "type-helper";
+	static String FILTER_CLASS = "type-filter";
+	static String DATABASE_CLASS = "type-database";
+	
+	String type() default HELPER_CLASS;
+	
+}
