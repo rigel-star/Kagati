418ba83b519ecc6e07f9b29dd156afeccc62831d
RigelStar <47748256+rigel-star@users.noreply.github.com>
2020-08-08 22:06:48 +0545

    minor changes

--- src/org/lemon/gui/Workspace.java
+++ src/org/lemon/gui/Workspace.java
@@ -14,16 +14,20 @@ import java.util.List;
 
 import javax.swing.JDesktopPane;
 
+import org.lemon.LemonObject;
 import org.lemon.utils.Pair;
 
 /**
  * Main background pane of application, which handles editing and drawing panels in application.
  * */
+@LemonObject(type = LemonObject.GUI_CLASS)
 public class Workspace extends JDesktopPane implements ComponentListener {
 	private static final long serialVersionUID = 1L;
 	
 	
 	private List<Node> nodes = new ArrayList<Node>();
+	private List<FilterControllable> fcontrollables = new ArrayList<>();
+	private List<FilterController> fcontrollers = new ArrayList<>();
 	
 	
 	public Workspace() {
@@ -32,7 +36,37 @@ public class Workspace extends JDesktopPane implements ComponentListener {
 		setBackground(new Color(160, 160, 160));
 		setVisible(true);
 		
+		fetchNodes();
+		
 		addComponentListener(this);
+	}
+	
+	
+	
+	@Override
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		var g2d = (Graphics2D) g;
+		
+		for(Component c: getComponents()) {
+			if(c instanceof ImageView) {
+				var view = (ImageView) c;
+				if(view.getConnection() != null) {
+					draw(g2d, new Pair(view, view.getConnection()));
+				}
+			}
+		}
+		
+		fcontrollables.stream().forEach(fc -> {
+			var node = fc.getNode();
+			drawNode(g2d, node);
+		});
+		
+		fcontrollers.stream().forEach(fc -> {
+			for(Node node: fc.getNodes()) {
+				drawNode(g2d, node);
+			}
+		});
 	}
 	
 	
@@ -42,74 +76,94 @@ public class Workspace extends JDesktopPane implements ComponentListener {
 	 * */
 	public void refresh() {
 		
-		List<ImageView> views = new ArrayList<ImageView>();
-		List<String> titles = new ArrayList<String>();
-		
 		for(Component c: getComponents()) {
-			if(c instanceof ImageView) {
-				
-				ImageView view = (ImageView) c;
-				
-				view.addComponentListener(this);
-				views.add((ImageView) view);
-				titles.add(((ImageView) view).getTitle());
-			}
+			c.addComponentListener(this);	
 		}
 		
-		for(ImageView v: views) {
-			v.setConOptions(titles, views);
+		if(fcontrollables.size() > 0) {
+			fcontrollers.stream().forEach(fc -> {
+				for(Node node: fc.getNodes()) {
+					node.addConnection(fcontrollables.get(0));
+				}
+			});
 		}
 		
 	}
 	
 	
-	@Override
-	public void paintComponent(Graphics g) {
-		super.paintComponent(g);
-		
+	/*fetch all the nodes*/
+	public void fetchNodes() {
 		for(Component c: getComponents()) {
-			if(c instanceof ImageView) {
-				var view = (ImageView) c;
-				if(view.getConnection() != null) {
-					draw(g, new Pair(view, view.getConnection()));
-				}
-			}
 			if(c instanceof FilterController) {
 				var con = (FilterController) c;
+				
+				if(!fcontrollers.contains(con))
+					fcontrollers.add(con);
+
 				for(Node node: con.getNodes()) {
-					if(!nodes.contains(node))
+					if(!nodes.contains(node)) {
 						nodes.add(node);
+					}
+					
+				}
+			}
+			else if(c instanceof FilterControllable) {
+				var fc = (FilterControllable) c;
+				
+				if(!fcontrollables.contains(fc))
+					fcontrollables.add(fc);
+				
+				if(!nodes.contains(fc.getNode())) {
+					nodes.add(fc.getNode());
 				}
 			}
 		}
-		
-		drawNodes(g);
+		System.out.println("Nodes: " + nodes.size());
+		System.out.println("Filter controllables: " + fcontrollables.size());
+		System.out.println("Filter controllers: " + fcontrollers.size());
 	}
+
 	
 	
-	
-	private void drawNodes(Graphics g) {
-		Graphics2D g2d = (Graphics2D) g;
-		g2d.setPaint(Color.white);
-		g2d.setStroke(new BasicStroke(3));
+	private void drawNode(Graphics2D g2d, Node node) {
+		g2d.setPaint(Color.yellow);
 		
-		for(Node node: nodes) {
-			g2d.fillOval((int) node.start.x, (int) node.start.y, 10, 10);
-			//g2d.fillOval((int) node.end.x, (int) node.end.y, 10, 10);
+		g2d.fillOval((int) node.start.x - 7, (int) node.start.y - 7, 15, 15);
+		
+		if(node.getParent() instanceof FilterControllable) {
 			
-			//var curve = new QuadCurve2D.Double((int) node.start.x, (int) node.start.y,
-			//									(int) node.mid.x, (int) node.mid.y,
-			//									(int) node.end.x, (int) node.end.y);
-			
-			//g2d.draw(curve);
 		}
+		else if(node.getParent() instanceof FilterController) {
+		
+			node.getConnections().forEach(c -> {
+					
+				g2d.setPaint(Color.white);
+				g2d.setStroke(new BasicStroke(2));
+				node.setEnd(node.start.midpoint(c.getNode().start));
+				var curve = new QuadCurve2D.Double((int) node.start.x, (int) node.start.y,
+													(int) node.mid.x, (int) node.mid.y,
+													(int) node.end.x, (int) node.end.y);
+				g2d.draw(curve);
+			});
+		}
+		
 	}
 	
+	
+	private void updateNodes() {
+		fcontrollables.forEach(fc -> {
+			fc.updateNode();
+		});
+		
+		fcontrollers.forEach(fc -> {
+			fc.updateNodes();
+		});
+		repaint();
+	}
 	
 	
 	/*drawing curve between two connections*/
-	private void draw(Graphics g, Pair p) {
-		Graphics2D g2d = (Graphics2D) g;
+	private void draw(Graphics2D g2d, Pair p) {
 		g2d.setPaint(Color.white);
 		g2d.setStroke(new BasicStroke(3));
 		
@@ -135,28 +189,27 @@ public class Workspace extends JDesktopPane implements ComponentListener {
 		
 	}
 	
-	
 
 	@Override
 	public void componentResized(ComponentEvent e) {
-		repaint();
+		updateNodes();
 	}
 
 	@Override
 	public void componentMoved(ComponentEvent e) {
-		repaint();
+		updateNodes();
 	}
 
 	@Override
 	public void componentShown(ComponentEvent e) {
-		repaint();
+		updateNodes();
 	}
 
 	@Override
 	public void componentHidden(ComponentEvent e) {
-		repaint();
+		updateNodes();
 	}
 	
-
+	
 	
 }
