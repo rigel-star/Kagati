92e170d212702e6c792016e03f73061e0d99961f
RigelStar <47748256+rigel-star@users.noreply.github.com>
2020-08-21 19:56:06 +0545

    minor changes

--- src/org/lemon/gui/menus/EditMenu.java
+++ src/org/lemon/gui/menus/EditMenu.java
@@ -1,21 +1,23 @@
 package org.lemon.gui.menus;
 
+import java.awt.Component;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JMenu;
 import javax.swing.JMenuItem;
 
-import org.lemon.gui.HSBController;
 import org.lemon.gui.ImageView;
 import org.lemon.gui.Workspace;
+import org.lemon.gui.filter.HSBController;
+import org.lemon.gui.filter.TextureController;
 
 public class EditMenu extends JMenu implements ActionListener {
 	private static final long serialVersionUID = 1L;
 
 	
 	private JMenu rotate;
-	private JMenuItem blur, crop, flipRight, flipHorizontal, hsbControl;
+	private JMenuItem blur, crop, flipRight, flipHorizontal, hsbControl, texture;
 	
 	private Workspace workspace;
 	
@@ -36,6 +38,7 @@ public class EditMenu extends JMenu implements ActionListener {
 		this.crop = new JMenuItem("Crop");
 		this.rotate = new JMenu("Rotate");
 		this.hsbControl = new JMenuItem("HSB");
+		this.texture = new JMenuItem("Texture");
 		
 		this.flipHorizontal = new JMenuItem("Flip Horizontal");
 		this.flipRight = new JMenuItem("Flip Right");
@@ -44,7 +47,10 @@ public class EditMenu extends JMenu implements ActionListener {
 	
 	private void addAll() {
 		
+		rotate.add(flipHorizontal);
+		rotate.add(flipRight);
 		add(rotate);
+		
 		add(crop);
 		
 		add(hsbControl);
@@ -52,28 +58,33 @@ public class EditMenu extends JMenu implements ActionListener {
 		
 		add(blur);
 		
-		this.rotate.add(flipHorizontal);
-		this.rotate.add(flipRight);
+		add(texture);
+		texture.addActionListener(this);
+		
 	}
 
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		
+		Component comp = null;
+		
 		if(e.getSource() == hsbControl) {
-			
-			HSBController hsb = null;
 			
 			if(workspace.getSelectedFrame() instanceof ImageView) {
 				var v = (ImageView) workspace.getSelectedFrame();
-				hsb = new HSBController(v, v.getImagePanel().getImage());
+				comp = new HSBController(v, v.getImagePanel().getImage());
 			}
-			
-			workspace.add(hsb);
-			workspace.fetchNodes();
-			workspace.refresh();
-			workspace.revalidate();
 		}
+		
+		else if(e.getSource() == texture) {
+			comp = new TextureController();
+		}
+		
+		workspace.add(comp);
+		workspace.fetchNodes();
+		workspace.refresh();
+		workspace.revalidate();
 	}
 	
 	
