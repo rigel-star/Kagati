ff5b3198b9bbdf5ab1633da5c4e2162feab9b9d2
RigelStar <47748256+rigel-star@users.noreply.github.com>
2020-08-21 21:34:40 +0545

    read comment in file for info

---
+++ src/org/lemon/gui/View.java
@@ -0,0 +1,13 @@
+package org.lemon.gui;
+
+import org.lemon.LemonObject;
+
+@LemonObject (type = LemonObject.HELPER_CLASS)
+
+/**
+ * View interface is methodless(for now) interface which is implemented by the classes which displays the image i.e 
+ * {@code ImageView, DrawingPanel}.
+ * */
+public interface View {
+
+}
