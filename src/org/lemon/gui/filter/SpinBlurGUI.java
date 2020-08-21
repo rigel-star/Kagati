8f5ac78014b355cf4edf6459b7b4de610c99aafa
RigelStar <47748256+rigel-star@users.noreply.github.com>
2020-08-21 21:40:49 +0545

    spin blur gui

---
+++ src/org/lemon/gui/filter/SpinBlurGUI.java
@@ -0,0 +1,17 @@
+package org.lemon.gui.filter;
+
+import java.awt.event.MouseAdapter;
+
+import org.lemon.gui.View;
+
+public class SpinBlurGUI extends MouseAdapter {
+	
+	
+	public SpinBlurGUI(View view) {
+		
+		
+		
+	}
+	
+	
+}
