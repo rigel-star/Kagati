c6207d90c5a301c357d666a3ffbb1ca2d03f9dac
RigelStar <47748256+rigel-star@users.noreply.github.com>
2020-08-22 16:04:30 +0545

    save changes toolbar

---
+++ src/org/lemon/gui/toolbars/SaveChangesToolBar.java
@@ -0,0 +1,35 @@
+package org.lemon.gui.toolbars;
+
+import java.awt.FlowLayout;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JToolBar;
+
+public class SaveChangesToolBar extends JToolBar {
+	private static final long serialVersionUID = 1L;
+	
+	
+	private JButton cancel, done;
+	
+	public SaveChangesToolBar() {
+		
+		init();
+		
+		setLayout(new FlowLayout(FlowLayout.RIGHT));
+		
+		add(cancel);
+		add(done);
+	}
+	
+	
+	private void init() {
+		
+		done = new JButton();
+		done.setIcon(new ImageIcon("icons/tick.png"));
+		
+		cancel = new JButton();
+		cancel.setIcon(new ImageIcon("icons/cross.png"));
+	}
+
+}
