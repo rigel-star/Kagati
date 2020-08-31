c2fd42fc4442af9291ba2c71b7b8a3dfbb46816a
RigelStar <47748256+rigel-star@users.noreply.github.com>
2020-08-31 16:07:02 +0545

    image gray scale algorithm re-implemented with optimization

--- src/org/lemon/filters/GrayImageFilter.java
+++ src/org/lemon/filters/GrayImageFilter.java
@@ -1,62 +1,24 @@
 package org.lemon.filters;
 
-import java.awt.image.DataBufferInt;
-import java.awt.image.Raster;
-
-import org.lemon.image.LImage;
-
 public class GrayImageFilter extends AbstractImageFilter {
-
-	
-	@Override
-	public void filter( LImage limage ) {
-		
-		byte[] data = limage.getPixels();
-		
-		int pixelLength = 3;
-		
-		if( limage.hasAlphaChannel() )
-			pixelLength = 4;
-		
-		int[] fdata = new int[data.length / pixelLength];
-		
-		final int len = fdata.length;
-		
-		for( int ind = 0; ind < len - pixelLength; ind += pixelLength ) {
-			
-			int argb = 0;
-			
-			if( !limage.hasAlphaChannel() )
-				argb += ( data[ind] + data[ind + 1] + data[ind + 2] );
-			else
-				argb = ( data[ind + 1] + data[ind + 2] + data[ind + 3]);
-			
-			fdata[ind] = process( argb );
-		}
-		
-		DataBufferInt bint = new DataBufferInt( fdata, len );
-		Raster ras = Raster.createRaster( null, bint, null );
-		
-		limage.getAsBufferedImage().setData( ras );
-	}
 	
 	
 	@Override
-	public int process( int pixel ) {
+	public int process( int rgb ) {
+        
+		int a = (int) ( ( rgb >> 24 ) & 0xff);
+        int b = (int) ( rgb & 0xff);
+        int g = (int) ( ( rgb >> 8 ) & 0xff);
+        int r = (int) ( ( rgb >> 16 ) & 0xff);
+        
+        final int lum = (int) ( b * 0.0722 + g * 0.2126 + r * 0.2126 );
 		
-		int alpha = ( pixel) & 0xff;
-		int red = ( pixel) & 0xff;
-		int green = ( pixel) & 0xff;
-		int blue = ( pixel ) & 0xff;
-		
-		final int lum = ( int )( 0.2126 * red + 0.7152 * green + 0.0722 * blue );
-		 
-        alpha = ( alpha << 24 );
-        red = ( lum << 16 );
-        green = ( lum << 8 );
-        blue = lum;
-		
-		return ( alpha + red + green + blue );
+        a = (rgb << 24 );
+        b = ( lum );
+        g = ( lum << 8 );
+        r = ( lum << 16 );
+        
+		return ( r + g + b + a );
 	}
 
 }
