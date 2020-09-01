80fab8263d0b80c27fa60e7bb0a4bd29cb7707f2
RigelStar <47748256+rigel-star@users.noreply.github.com>
2020-09-01 16:18:47 +0545

    super class change from AbstractImageFilter to SingleRGBFilter

--- src/org/lemon/filters/GrayImageFilter.java
+++ src/org/lemon/filters/GrayImageFilter.java
@@ -1,15 +1,15 @@
 package org.lemon.filters;
 
-public class GrayImageFilter extends AbstractImageFilter {
+public class GrayImageFilter extends SingleRGBFilter {
 	
 	
 	@Override
-	public int process( int rgb ) {
+	public int processRGB( int rgb ) {
         
-		int a = (int) ( ( rgb >> 24 ) & 0xff);
-        int b = (int) ( rgb & 0xff);
-        int g = (int) ( ( rgb >> 8 ) & 0xff);
-        int r = (int) ( ( rgb >> 16 ) & 0xff);
+		int a = (int) ( rgb & 0xFF000000);
+        int b = (int) ( rgb & 0xFF);
+        int g = (int) ( ( rgb >> 8 ) & 0xFF);
+        int r = (int) ( ( rgb >> 16 ) & 0xFF);
         
         final int lum = (int) ( b * 0.0722 + g * 0.2126 + r * 0.2126 );
 		
