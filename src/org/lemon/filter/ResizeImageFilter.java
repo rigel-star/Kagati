171a4e2e8c530258cfea7521ef528246bd2f331f
RigelStar <47748256+rigel-star@users.noreply.github.com>
2020-09-21 22:16:38 +0545

    image not resizing issue fixed

--- src/org/lemon/filter/ResizeImageFilter.java
+++ src/org/lemon/filter/ResizeImageFilter.java
@@ -6,17 +6,33 @@ import java.awt.image.BufferedImage;
 import org.lemon.image.ImageGraphics;
 import org.lemon.image.LImage;
 
+/**
+ * 
+ * Resize image to the size of desired bounds.
+ * 
+ * */
 public class ResizeImageFilter extends AbstractImageFilter {
 	
-	
-	private int w, h;
+	private int w = 0, h = 0;
 	
 	/**
 	 * 
-	 * Resize image to the size of desired bounds.
+	 * Constructs {@code ResizeImageFilter} with 
+	 * default width and height value of 0.
 	 * 
-	 * @param newW new width for the image
-	 * @param newH new height for the image
+	 * */
+	public ResizeImageFilter() {
+		
+	}
+	
+	
+	/**
+	 * 
+	 * Constructs {@code ResizeImageFilter} with new height
+	 * and width.
+	 * 
+	 * @param newW 		new width
+	 * @param newH 		new height
 	 * 
 	 * */
 	public ResizeImageFilter( int newW, int newH ) {
@@ -28,13 +44,40 @@ public class ResizeImageFilter extends AbstractImageFilter {
 	@Override
 	public LImage filter( LImage limage ) {
 		
+		if( w == 0)
+			w = limage.width;
+		if( h == 0)
+			h = limage.height;
+		
 		BufferedImage src = limage.getAsBufferedImage();
-		LImage out = ImageGraphics.createImage( w, h, src.getType() );
+		LImage out = ImageGraphics.createImage( w, h, LImage.DEFAULT );
 		
 		Graphics2D g2 = out.getAsBufferedImage().createGraphics();
-		g2.drawImage( src, 0, 0, null );
+		g2.drawImage( src, 0, 0, w, h, null );
 		
 		return out;
+	}
+	
+	
+	/**
+	 * 
+	 * Set new width to resize image to.
+	 * @param w 	new width
+	 * 
+	 * */
+	public void setNewWidth( int w ) {
+		this.w = w;
+	}
+	
+	
+	/**
+	 * 
+	 * Set new height to resize image to.
+	 * @param h 	new height
+	 * 
+	 * */
+	public void setNewHeight( int h ) {
+		this.h = h;
 	}
 
 }
