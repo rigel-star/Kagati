3269dbb833702665f4f4712c152a13f01bdb4d36
RigelStar <47748256+rigel-star@users.noreply.github.com>
2020-09-24 20:31:05 +0545

    Node is interface now and all of its functions are moved to lemon.gui.node.NodePt.

--- src/org/lemon/gui/Node.java
+++ src/org/lemon/gui/Node.java
@@ -1,68 +1,13 @@
 package org.lemon.gui;
 
-import java.awt.Shape;
-import java.awt.geom.Ellipse2D;
-import java.util.Stack;
-
-import javax.swing.JComponent;
-
-import org.lemon.lang.LemonObject;
-import org.lemon.math.Vec2d;
-
-@LemonObject(type = LemonObject.HELPER_CLASS)
-public class Node {
+/**
+ * 
+ * This have two sub-classes (maybe for now), 
+ * {@code ControllableNode} & {@code ControllerNode}.
+ * 
+ * @author Ramesh Poudel
+ * 
+ * */
+public interface Node {
 	
-	public Vec2d start = null, end = null, mid = null;
-	private JComponent  parent;
-	
-	private Stack<ControllableNode> cons = new Stack<>();
-	
-	
-	public Node(Vec2d start, Vec2d end) {
-		this(start, end, null);
-	}
-	
-	
-	public Node(Vec2d start, Vec2d end, JComponent parent) {
-		this.parent = parent;
-		this.start = start;
-		this.end = end;
-		
-		if(this.end != null)
-			this.mid = start.midpoint(end);
-	}
-	
-	
-	public void setStart(Vec2d start) {
-		this.start = start;
-	}
-	
-	
-	public void setEnd(Vec2d end) {
-		this.end = end;
-		if(this.end != null)
-			this.mid = start.midpoint(end);
-	}
-	
-	
-	public void addConnection(ControllableNode controllable) {
-		cons.push(controllable);
-		System.out.println("Added!");
-		System.out.println("Count: " + cons.size());
-	}
-	
-	
-	public JComponent getParent() {
-		return parent;
-	}
-	
-	
-	public Stack<ControllableNode> getConnections() {
-		return cons;
-	}
-	
-	
-	public Shape getDrawable() {
-		return new Ellipse2D.Double(start.x - 5, start.y - 5, 10, 10);
-	}	
 }
