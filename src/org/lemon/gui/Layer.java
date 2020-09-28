f1b4a8321c09970e26fefcc5f6b189bc4d702af1
RigelStar <47748256+rigel-star@users.noreply.github.com>
2020-09-28 16:30:27 +0545

    now a super class for different types of layers

--- src/org/lemon/gui/Layer.java
+++ src/org/lemon/gui/Layer.java
@@ -1,92 +1,36 @@
-package org.lemon.gui.layers;
+package org.lemon.gui;
 
 import javax.swing.JComponent;
 
-
-public class Layer {
+/**
+ * 
+ * Layer Interface.
+ * 
+ * */
+public interface Layer {
 	
-	public static final int BACKGROUND_LAYER = 0;
-	public static final int NORMAL_LAYER = 1;
-	
-	private JComponent comp = null;
-	private String title = null;
-	
-	private int property = NORMAL_LAYER;
-	public int id;
+	/**
+	 * 
+	 * @return 		{@code JComponent} attached with this {@link Layer}.
+	 * 
+	 * */
+	public JComponent getLayerComponent();
 	
 	
 	/**
 	 * 
-	 * @param comp 		the image container which holds the image
+	 * @return Title of this {@link Layer}
 	 * 
 	 * */
-	public Layer( JComponent comp ) {
-		this( comp, "Layer", NORMAL_LAYER );
-	}
+	public String getTitle();
 	
 	
 	/**
 	 * 
-	 * @param comp 		the image container which holds the image
-	 * @param title 	title for the {@code Layer}
+	 * Set title of this {@link Layer}.
+	 * @param title 	New title.
 	 * 
 	 * */
-	public Layer( JComponent comp, String title ) {
-		this( comp, title, NORMAL_LAYER );
-	}
+	public void setTitle( String title );
 	
-	
-	/**
-	 * 
-	 * @param comp 		the image container which holds the image
-	 * @param property 	property of the {@code Layer}
-	 * 
-	 * */
-	public Layer( JComponent comp, int property ) {
-		this( comp, "Layer", NORMAL_LAYER );
-	}
-	
-	
-	/**
-	 * 
-	 * @param comp 		the image container which holds the image
-	 * @param title 	title for the {@code Layer}
-	 * @param property 	Property of the {@code Layer}
-	 * 
-	 * */
-	public Layer( JComponent comp, String title, int property ) {
-		this.comp = comp;
-		this.title = title;
-		this.property = property;
-	}
-
-	
-	public void setLayerComponent( JComponent comp ) {
-		this.comp = comp;
-	}
-
-	
-	public void setTitle( String  title ) {
-		this.title = title;
-	}
-	
-	
-	public void setProperty( int property ) {
-		this.property = property;
-	}
-	
-	
-	public JComponent getLayerComponent() {
-		return comp;
-	}
-	
-	
-	public String getTitle() {
-		return title;
-	}
-	
-	
-	public int getProperty() {
-		return property;
-	}
 }
