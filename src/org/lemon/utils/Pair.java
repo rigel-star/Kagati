dbcacd4295bece1528250f79646ab1d33cd164dd
RigelStar <47748256+rigel-star@users.noreply.github.com>
2021-02-07 16:30:30 +0545

    Add files via upload

--- src/org/lemon/utils/Pair.java
+++ src/org/lemon/utils/Pair.java
@@ -2,19 +2,19 @@ package org.lemon.utils;
 
 import javax.swing.JComponent;
 
-import org.lemon.math.Vec2d;
+import org.lemon.math.Vec2;
 
 public class Pair {
 
 	private JComponent firstComp, secondComp;
-	private Vec2d firstPt, secondPt;
+	private Vec2 firstPt, secondPt;
 	
 	public Pair(JComponent comp1, JComponent comp2) {
 		this.firstComp = comp1;
 		this.secondComp = comp2;
 		
-		this.firstPt = new Vec2d(comp1.getLocation().x, comp1.getLocation().y);
-		this.secondPt = new Vec2d(comp2.getLocation().x, comp2.getLocation().y);
+		this.firstPt = new Vec2(comp1.getLocation().x, comp1.getLocation().y);
+		this.secondPt = new Vec2(comp2.getLocation().x, comp2.getLocation().y);
 	}
 	
 	
@@ -26,11 +26,11 @@ public class Pair {
 		return this.secondComp;
 	}
 	
-	public Vec2d getFirstPoint() {
+	public Vec2 getFirstPoint() {
 		return this.firstPt;
 	}
 	
-	public Vec2d getSecondPoint() {
+	public Vec2 getSecondPoint() {
 		return this.secondPt;
 	}
 	
--- src/org/lemon/utils/Utils.java
+++ src/org/lemon/utils/Utils.java
@@ -4,14 +4,36 @@ import java.awt.image.BufferedImage;
 import java.awt.image.ColorModel;
 import java.awt.image.WritableRaster;
 
+import org.lemon.image.LImage;
+
 public class Utils {
 
 	
-	public static BufferedImage getImageCopy(BufferedImage sr) {
+	/**
+	 * 
+	 * Get copy of an image with same bounds and data.
+	 * @param sr		image to copy
+	 * @return Copied image.
+	 * 
+	 * */
+	public static BufferedImage getImageCopy( BufferedImage sr ) {
 		ColorModel cm = sr.getColorModel();
 		boolean iap = sr.isAlphaPremultiplied();
-		WritableRaster raster = sr.copyData(sr.getRaster().createCompatibleWritableRaster());
-		return new BufferedImage(cm, raster, iap, null);
+		WritableRaster raster = sr.copyData( sr.getRaster().createCompatibleWritableRaster() );
+		return new BufferedImage( cm, raster, iap, null );
+	}
+	
+	
+	/**
+	 * 
+	 * Get copy of an image with same bounds but not 
+	 * same data.
+	 * @param sr		image to copy
+	 * @return Blank image.
+	 * 
+	 * */
+	public static LImage getBlankCopy( LImage sr ) {
+		return new LImage( new BufferedImage( sr.width, sr.height, sr.getAsBufferedImage().getType()));
 	}
 	
 }
